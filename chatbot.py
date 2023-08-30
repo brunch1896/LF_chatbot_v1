24f45a2b486119639ac4d9579ba8a19f8bab59a8
brunch1896 <brunch_zhu@163.com>
2023-08-30 16:10:31 +0800

    chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -10,6 +10,7 @@ import streamlit as st
 # openai_api_key = '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'
 
 st.title("💬 颖鸿之家问答机器人")
+openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "你想问什么问题？"}]
 
@@ -17,11 +18,11 @@ for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
 if prompt := st.chat_input():
-    # if not openai_api_key:
-    #     st.info("Please add your OpenAI API key to continue.")
-    #     st.stop()
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
 
-    openai.api_key = "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"
+    openai.api_key = openai_api_key
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
